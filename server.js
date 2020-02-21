44ca5e68c2224e82126524a2abd14420e73c85be
Jie Yan <jieyan620@gmail.com>
2020-02-20 16:37:27 -0800

    added htmls

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 
 prompt([
   {
-    type:'input',
-    name:'userName',
-    message:'What is your name?'
+    type: 'input',
+    name: 'userName',
+    message: 'What is your name?'
   },
   {
     type: 'input',
     name: 'email',
     message: 'What is your email?'
-  }, 
+  },
   {
     type: 'input',
     name: 'department',
@@ -27,24 +27,26 @@ prompt([
     name: 'github',
     message: 'What is your GitHub username?'
   },
+  {
+    type: 'list',
+    name: 'type',
+    message: 'Which types do you belong to?',
+    choices:['engineer','intern','manager']
+  }
 ])
-.then(response=>{
-  // console.log(response)
-const info = `
-* Name : ${response.userName}
-* Email : ${response.email}
-* Department : ${response.department}
-* University : ${response.university}
-* GitHub username : ${response.github}
-`
-  // const infolist =[]
-  fs.readFile('info.json', 'utf8', (e, data) => {
-    if (e) { console.log(e) }
-  const infolist = JSON.parse(data)
-  infolist.push(info)
+  .then(response => {
+    // console.log(response)
+//     const info = `Name : ${response.userName}Email : ${response.email}Department : ${response.department}University : ${response.university}GitHub username : ${response.github}
+// `
+    // const infolist = []
+    fs.readFile('info.json', 'utf8', (e, data) => {
+      if (e) { console.log(e) }
+      const infolist = JSON.parse(data)
+      infolist.push(response)
 
-  fs.writeFile('info.json', JSON.stringify(infolist), e => e ? console.log(e) : null)
+      fs.writeFile('info.json', JSON.stringify(infolist), e => e ? console.log(e) : null)
 
-})
- .catch(e => console.error(e))
+    })
+  })
+  .catch(e => console.error(e))
 
